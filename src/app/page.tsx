2aee0f82312e590866ab917e5912e1965e4d66c2
nagi4205 <nagisome1234@gmail.com>
2023-12-17 16:41:37 +0900

    Updata: app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,7 +19,7 @@ function Home({
       <section className='mt-9 flex flex-col gap-10'>
         <p className='no-result'>No threads found</p>
 
-        <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7'>
+        <article className='flex w-full flex-col rounded-xl bg-dark-2 p-7 border'>
           <div className='flex items-start justify-between'>
             <div className='flex w-full flex-1 flex-row gap-4'>
               <div className='flex flex-col items-center'>
@@ -121,6 +121,115 @@ function Home({
           isComment={isComment}
         /> */}
           </div>
+        </article>
+
+        <article
+          // className={`flex w-full flex-col rounded-xl ${
+          //   isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"
+          // }`}
+          className='flex w-full flex-col rounded-xl'
+        >
+          <div className='flex items-start justify-between'>
+            <div className='flex w-full flex-1 flex-row gap-4'>
+              <div className='flex flex-col items-center'>
+                {/* <Link href={`/profile/${author.id}`} className="relative h-11 w-11"> */}
+                <Link href='/' className='relative h-11 w-11'></Link>
+
+                <div className='thread-card_bar' />
+              </div>
+
+              <div className='flex w-full flex-col'>
+                {/* <Link href={`/profile/${author.id}`} className="w-fit"> */}
+                <div className='flex justify-between'></div>
+                <Link href='/' className='w-fit'>
+                  <h4 className='cursor-pointer text-base-semibold text-light-1'>
+                    {/* {author.name} */}
+                    原田泰造
+                  </h4>
+                </Link>
+                <div>残り時間：39分</div>
+
+                <p className='mt-2 text-small-regular text-light-2'>
+                  インド映画
+                </p>
+
+                {/* <div className={`${isComment && "mb-10"} mt-5 flex flex-col gap-3`}> */}
+                <div className='mt-5 flex flex-col gap-3'>
+                  <div className='flex gap-3.5'>
+                    <Image
+                      src='/assets/heart-gray.svg'
+                      alt='heart'
+                      width={24}
+                      height={24}
+                      className='cursor-pointer object-contain'
+                    />
+                    <Link href={`/thread/`}>
+                      <Image
+                        src='/assets/reply.svg'
+                        alt='heart'
+                        width={24}
+                        height={24}
+                        className='cursor-pointer object-contain'
+                      />
+                    </Link>
+                    <Image
+                      src='/assets/repost.svg'
+                      alt='heart'
+                      width={24}
+                      height={24}
+                      className='cursor-pointer object-contain'
+                    />
+                    <Image
+                      src='/assets/share.svg'
+                      alt='heart'
+                      width={24}
+                      height={24}
+                      className='cursor-pointer object-contain'
+                    />
+                  </div>
+
+                  {/* {isComment && comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+                  </p>
+                </Link>
+              )} */}
+                </div>
+              </div>
+            </div>
+
+            {/* <DeleteThread
+          threadId={JSON.stringify(id)}
+          currentUserId={currentUserId}
+          authorId={author.id}
+          parentId={parentId}
+          isComment={isComment}
+        /> */}
+          </div>
+          {/* 
+      {!isComment && comments.length > 0 && (
+        <div className="ml-1 mt-3 flex items-center gap-2">
+          {comments.slice(0, 2).map((comment, index) => (
+            <Image
+              key={index}
+              src={comment.author.image}
+              alt={`user_${index}`}
+              width={24}
+              height={24}
+              className={`${index !== 0 && "-ml-5"} rounded-full object-cover`}
+            />
+          ))}
+
+          <Link href={`/thread/${id}`}>
+            <p className="mt-1 text-subtle-medium text-gray-1">
+              {comments.length} repl{comments.length > 1 ? "ies" : "y"}
+            </p>
+          </Link>
+        </div>
+      )} */}
+
+          {/* isComment && communityの部分カットしています。 */}
         </article>
       </section>
     </>
