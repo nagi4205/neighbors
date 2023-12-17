3567cd4d39497aaacd27a9a21e15bb18a6ddf653
nagi4205 <nagisome1234@gmail.com>
2023-12-17 14:03:05 +0900

    fix: change Rightsidebar layout

--- src/components/shared/RightSidebar.tsx
+++ src/components/shared/RightSidebar.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const RightSidebar = () => {
-  return <div className='max-md:hidden'>Rightsidebar</div>;
+  return (
+    <div className='max-w-[300px] break-words overflow-hidden whitespace-normal max-lg:hidden'>
+      Rightsidebar!Rightsidebar!Rightsidebar!Rightsidebar!Rightsidebar!Rightsidebar!Rightsidebar!Rightsidebarっっっっっっっs
+    </div>
+  );
 };
 
 export default RightSidebar;
