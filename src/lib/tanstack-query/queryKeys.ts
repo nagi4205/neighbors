1d5a4223a419470f4714d8bfe7f2634fd9ec98e2
nagi4205 <nagisome1234@gmail.com>
2023-12-18 08:21:04 +0900

    feat: add queryKeys for tanstack-query

--- src/lib/tanstack-query/queryKeys.ts
+++ src/lib/tanstack-query/queryKeys.ts
@@ -1,6 +1,9 @@
 export enum QUERY_KEYS {
   //  GEOLOCATION
   GET_GEOLOCATION = 'getGeolocation',
+  GET_REVERSE_GEOCODING = 'getReverseGeocoding',
   //  SEARCH KEYS
-  GET_POSTS_BY_GEOLOCATION = 'getPostsByGeoocation',
+  GET_POSTS_BY_GEOLOCATION = 'getPostsByGeolocation',
+  GET_POSTS_BY_FOLLOWINGUSERS = 'getPostsByFollowingUers',
+  GET_COMMUNITIES_BY_GEOLOCATION = 'getCommunitiesByGeoocation',
 }
