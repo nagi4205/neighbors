05919293187d044cc3ca14e4bf0693cf350d15ed
nagi4205 <nagisome1234@gmail.com>
2023-12-23 17:56:43 +0900

    fix: fix a loading component file
    ローディングコンポーネントで読み込むsvgファイルをドキュメントのクラスリストを参照して区別するコードに修正

--- src/components/shared/Loader.tsx
+++ src/components/shared/Loader.tsx
@@ -1,13 +1,32 @@
-const Loader = () => (
-  <div className='flex justify-center items-center w-full text-black'>
-    <img
-      src='/assets/loader.svg'
-      alt='loader'
-      width={24}
-      height={24}
-      className='animate-spin'
-    />
-  </div>
-);
+'use client';
+
+import { useEffect, useState } from 'react';
+
+type themeProps = 'light' | 'dark';
+
+const Loader = () => {
+  const [theme, setTheme] = useState<themeProps>('light');
+
+  useEffect(() => {
+    const isDarkMode = document.documentElement.classList.contains('dark');
+    setTheme(isDarkMode ? 'dark' : 'light');
+  }, []);
+
+  return (
+    <div className='flex justify-center items-center w-full'>
+      <img
+        src={
+          theme === 'dark'
+            ? '/assets/loader-dark-theme.svg'
+            : '/assets/loader-light-theme.svg'
+        }
+        alt='loader'
+        width={24}
+        height={24}
+        className='animate-spin'
+      />
+    </div>
+  );
+};
 
 export default Loader;
